916809d3a8b162273e04c822c27927ea8b61a770
Cary Goeller <carygoeller@gmail.com>
2017-06-05 14:15:27 +0200

    simple update

--- Script.R
+++ Script.R
@@ -4,4 +4,6 @@ blogdown::new_post("First New Post (in plain Markdown)")
 
 blogdown::new_post("Second New Post (in R Markdown)", rmd = TRUE)
 
+blogdown::install_theme("dim0627/hugo_theme_solit")
+
 blogdown::serve_site()
